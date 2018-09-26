ebfe3b8ebd7b992a3c34883c062fcd3f1370087b
Marek Tutaj <mtutaj@mcw.edu>
2018-09-26 08:29:23 -0500

    tuned up main script

--- src/main/dist/runOnReed.sh
+++ src/main/dist/runOnReed.sh
@@ -4,10 +4,10 @@
 . /etc/profile
 APPNAME=miRnaPipeline
 
-APPDIR=/home/rgddata/pipelines/$APPNAME
+APPDIR=/home/rgddata/pipelines/${APPNAME}_on_reed
 cd $APPDIR
 
 DB_OPTS="-Dspring.config=$APPDIR/../properties/reed.xml"
 LOG4J_OPTS="-Dlog4j.configuration=file://$APPDIR/properties/log4j.properties"
 declare -x "MI_RNA_PIPELINE_OPTS=$DB_OPTS $LOG4J_OPTS"
-bin/$APPNAME "$@"
+bin/$APPNAME -Xmx100G --load --stats "$@"
